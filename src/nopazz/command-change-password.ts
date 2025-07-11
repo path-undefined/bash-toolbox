2337fcc973971f10e0a0d5609ddf886ef3216334
path-undefined <path.undefined@gmail.com>
2025-07-11 10:25:10 +0200

    Minor fixes.

--- src/nopazz/command-change-password.ts
+++ src/nopazz/command-change-password.ts
@@ -1,10 +1,12 @@
 import { askForSecret } from "../common/console-input";
-import { printLine } from "../common/console-output";
+import { printEmptyLine, printLine } from "../common/console-output";
 import { AccountManager } from "./account-manager";
 
 export async function changeMainPassword(args: string[], accountManager: AccountManager) {
   const newMainPassword = await askForSecret("New main password: ");
   accountManager.setMainPassword(newMainPassword);
+
+  printEmptyLine();
 
   printLine("Main password successfully updated!");
 }
